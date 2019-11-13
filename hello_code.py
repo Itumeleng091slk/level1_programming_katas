d1e3775641006ca787b957aad0fe2d5916631af4
Zweli Mthethwa <zwescientist@gmail.com>
2019-11-13 12:44:32 +0200

    Finalised solutions

--- hello_code.py
+++ hello_code.py
@@ -1,5 +1,5 @@
 def hello(name):
     return "Hello" + " " + name + "!"
 #test
-user_name = input("Enter your name: ")
-print(hello(user_name))
+#user_name = input("Enter your name: ")
+#print(hello(user_name))
