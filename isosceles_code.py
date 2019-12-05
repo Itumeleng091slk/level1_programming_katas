521e2842bbb63292e45b6b46e790808694d829b1
zwelisha <36135871+zwelisha@users.noreply.github.com>
2019-12-05 17:36:58 +0200

    Removed input taking and processing lines.

--- isosceles_code.py
+++ isosceles_code.py
@@ -3,5 +3,4 @@ def isosceles(n):
     for i in range(1,n+1):
         print(" " * (n-i) + "#" * h_count)
         h_count += 2
-n = int(input("Enter the number of rows for your isosceles triangle: "))
-isosceles(n)
+
