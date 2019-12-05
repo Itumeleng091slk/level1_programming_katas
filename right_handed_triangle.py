6d479f7b92aab8857be4a29d3ab24e94784ee46a
zwelisha <36135871+zwelisha@users.noreply.github.com>
2019-12-05 17:39:33 +0200

    Removing the line to take an input.

--- right_handed_triangle.py
+++ right_handed_triangle.py
@@ -2,5 +2,3 @@ def triangle(num_rows):
     for row_count in range(abs(num_rows) + 1):
         print("#" * row_count)
 
-row_number = int(input("Enter your triangle number: "))
-triangle(row_number)
